f85cb62022d2a891cc6912b2e89ecdaafe829faf
A4-Tacks <wdsjxhno1001@163.com>
2025-05-24 15:40:25 +0800

    Add InTrue

--- src/lib.rs
+++ src/lib.rs
@@ -34,3 +34,47 @@ impl ToTrue for bool {
         }
     }
 }
+
+/// ```
+/// # use to_true::InTrue;
+/// let mut state = false;
+/// let mut n = 0;
+///
+/// assert_eq!(state.in_true(|| n += 1), None);
+/// assert_eq!((n, state), (0, true));
+///
+/// assert_eq!(state.in_true(|| n += 1), Some(()));
+/// assert_eq!((n, state), (1, true));
+///
+/// assert_eq!(state.in_false(|| n += 1), None);
+/// assert_eq!((n, state), (1, false));
+///
+/// assert_eq!(state.in_false(|| n += 1), Some(()));
+/// assert_eq!((n, state), (2, false));
+/// ```
+pub trait InTrue: Sized {
+    /// Run `f` when `*self == true`, then assign `*self` to `true`
+    fn in_true<F, R>(&mut self, f: F) -> Option<R>
+    where F: FnOnce() -> R;
+
+    /// Run `f` when `*self == false`, then assign `*self` to `false`
+    fn in_false<F, R>(&mut self, f: F) -> Option<R>
+    where F: FnOnce() -> R;
+}
+impl InTrue for bool {
+    fn in_true<F, R>(&mut self, f: F) -> Option<R>
+    where F: FnOnce() -> R,
+    {
+        let old = *self;
+        *self = true;
+        old.then(f)
+    }
+
+    fn in_false<F, R>(&mut self, f: F) -> Option<R>
+    where F: FnOnce() -> R,
+    {
+        let old = *self;
+        *self = false;
+        (!old).then(f)
+    }
+}
